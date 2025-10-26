3ed584750016cd9ec4f8c456c2897b127102ddf0
MasakiSatsumabayashi <209392348+satsumabayashi-git@users.noreply.github.com>
2025-10-26 12:27:34 +0000

    [Patch] 本番環境のテスト準備2

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
-  belongs_to :category
+  # belongs_to :category
   has_many :post_comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
