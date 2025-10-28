268b39a16e3692cb19a3128d4444c7f6a1e00ebe
MasakiSatsumabayashi <209392348+satsumabayashi-git@users.noreply.github.com>
2025-10-28 10:23:17 +0000

    [Finish] デプロイ完了ph1

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,10 +30,60 @@ Nyaan = User.find_or_create_by!(email: "nyaan@example.com") do |user|
   user.favorite_animal = "サーバル"
 end
 
-Post.find_or_create_by!(body: "さかな") do |post|
+Post.find_or_create_by!(body: "何の幼虫でしょうか？お尻に角？が生えてました。") do |post|
   post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
-  post.date = "２０２５年１０月"
-  post.place = "動物園"
+  post.date = "２０２５年９月"
+  post.place = "東京都大田区、倉庫街"
   post.user_id = 1
-  post.category_id = 1
+  post.category_id = 2
+end
+
+Post.find_or_create_by!(body: "白いハトがいました！皆さんは見かけたことありますか？") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+  post.date = "２０２３年（たぶん）"
+  post.place = "神奈川県川崎市、駅前"
+  post.user_id = 2
+  post.category_id = 3
+end
+
+Post.find_or_create_by!(body: "路地裏でタヌキ？を見かけました！アライグマかもしれません…") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
+  post.date = "２０２４年１０月"
+  post.place = "神奈川県、住宅街"
+  post.user_id = 2
+  post.category_id = 6
+end
+
+# Category.find_or_create_by!(name: "不明") do |post|
+  
+# end
+
+# Category.find_or_create_by!(name: "昆虫・その他の虫") do |post|
+  
+# end
+
+# Category.find_or_create_by!(name: "鳥類") do |post|
+  
+# end
+
+# Category.find_or_create_by!(name: "魚類・水中の生物") do |post|
+  
+# end
+
+# Category.find_or_create_by!(name: "爬虫類・両生類") do |post|
+  
+# end
+
+# Category.find_or_create_by!(name: "哺乳類") do |post|
+  
+# end
+
+# bookmarks = Bookmark.create!(
+#   [
+#     {user_id: 2, post_id: 1},
+#     {user_id: 2, post_id: 3},
+#     {user_id: 3, post_id: 1},
+#   ]
+# )
+  
 end
