4d2a7a8708f432b6dcd95da7cfc511e22659c958
MasakiSatsumabayashi <209392348+satsumabayashi-git@users.noreply.github.com>
2026-05-06 16:59:34 +0000

    bootstrapの表示確認

--- Gemfile
+++ Gemfile
@@ -69,3 +69,4 @@ gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
 gem "importmap-rails", "~> 2.2"
+gem 'bootstrap', '~> 4.5.0'
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,7 @@ pin "application"
 
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+pin "bootstrap" # @4.6.2
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "jquery" # @3.7.1
+pin "popper.js" # @1.16.1
